edfc34bb45e0b8667a06fae50677357bb47bb71f
Caleb <theldsrm@gmail.com>
2018-11-23 23:07:51 -0700

    Trying to incorporate dbpromise.js into dbhelper

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,5 +1,16 @@
 //import dbPromise from './dbpromise';
 
+function openDB() {
+    return idb.open('mws-restaurant-project-db', 1, function(upgradeDb) {
+        // case statement recommended by Jake in IDB lecture
+        switch (upgradeDb.oldVersion) {
+            case 0:
+                // use a property id as a keypath
+                upgradeDb.createObjectStore('restaurants', { keyPath: 'id' });
+        }
+    })
+}
+
 /**
  * Common database helper functions.
  */
@@ -24,6 +35,56 @@ class DBHelper {
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
+    // A separate dbpromise file was suggested in Alexandro Perez's walkthrough, but I would rather put the database here.
+    /******Start of code from dbpromise.js (also included openDB code above) ********/
+    static iniitializeDB () {
+    this.db = openDB()
+
+  }
+    /**
+     * Save a restaurant or array of restaurants into idb, using promises.
+     * can take an array of restaurants or a single restaurant
+     */
+    static putRestaurants(restaurants) {
+        if (!restaurants.push) restaurants = [restaurants];
+        return this.dbp.then((db) => {
+            // specify a read/write transaction for restaurants
+            const store = db.transaction('restaurants', 'readwrite').objectStore('restaurants');
+            // resolves only if the array of promises resolves
+            Promise.all(restaurants.map(networkRestaurant => {
+                // returns this promise on each element of the array
+                // if we get a resataurant back from index db then it was already saved
+                return store.get(networkRestaurant.id).then(idbRestaurant => {
+                    // if it doesn't already exist in db then restaurant is undefined
+                    // (works as a short circuit conditional statement)
+                    // or if we get newly updated restaurant information (don't update with old info)
+                    if (!idbRestaurant || networkRestaurant.updatedAt > idbRestaurant.updatedAt) {
+                        // go ahead and store the restaurant in idb
+                        return store.put(networkRestaurant);
+                    }
+                });
+            })).then(function () {
+                // resolves if all promises resolve successfully (true if everthing is ok)
+                return store.complete;
+            });
+        });
+    }
+
+    /**
+     * Get a restaurant, by its id (optional), or all stored restaurants in idb using promises.
+     * If no argument is passed, all restaurants will returned.
+     */
+    getRestaurants(id = undefined) {
+        return this.dbp.then(db => {
+            // we are reading (that's the default), not read/write
+            const store = db.transaction('restaurants').objectStore('restaurants');
+            // if there is an id (convert a string to number)
+            if (id) return store.get(Number(id));
+            // if there is no id
+            return store.getAll();
+        });
+    }
+  /******End of code from dbpromise.js ********/
   /**
    * Fetch all restaurants.
    */
@@ -32,11 +93,13 @@ class DBHelper {
     xhr.open('GET', `${DBHelper.API_URL}/restaurants`);
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
-        const restaurants = JSON.parse(xhr.responseText); //we are only getting an array now
-        // TODO: Fix dbPromise database stuff
-        //dbPromise.putRestaurants(restaurants);// store the restaurants in the db
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
+          const restaurants = JSON.parse(xhr.responseText); //we are only getting an array now
+          // TODO: Fix dbPromise database stuff
+          this.putRestaurants(restaurants).then(restaurants => {
+              callback(null, restaurants);// store the restaurants in the db
+              //callback(null, restaurants);
+          });
+      }  else { // Oops!. Got an error from server.
         const error = (`Request failed. Returned status of ${xhr.status}`);
         callback(error, null);
       }
